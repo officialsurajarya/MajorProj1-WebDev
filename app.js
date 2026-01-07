1cfb4fbccd68c075dcf1f84316d8b156952b44b6
officialsurajarya <officialsurajarya@gmail.com>
2026-01-07 21:00:36 +0530

    Updated validdation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,6 +34,16 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 // Index Route
 app.get(
   "/listings",
@@ -57,22 +68,12 @@ app.get(
   })
 );
 
+// Create Route
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send Valid Data For listing");
-    }
     const newListing = new Listing(req.body.listing);
-    if (!newListing.title) {
-      throw new ExpressError(400, "Title is missing!");
-    }
-    if (!newListing.description) {
-      throw new ExpressError(400, "Description is missing!");
-    }
-    if (!newListing.locat) {
-      throw new ExpressError(400, "Title is missing!");
-    }
     await newListing.save();
     res.redirect("/listings");
   })
@@ -91,10 +92,8 @@ app.get(
 // update Route
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(404, "Send Valid Data For listing");
-    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
--- schema.js
+++ schema.js
@@ -4,9 +4,9 @@ module.exports.listingSchema = Joi.object({
   listing: Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
-    location: Joi.string.required,
-    country: Joi.string.required,
-    price: Joi.number.required.min(0),
-    image: Joi.string.allow("", null),
-  }).required,
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    image: Joi.string().allow("", null),
+  }).required(),
 });
